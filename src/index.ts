a8c4c1538d1b7b5229844e95b28691031bdf2c32
Franz <franzj.pretorius.2003@gmail.com>
2024-06-10 13:48:04 +0200

    Strings mini challenge

--- src/index.ts
+++ src/index.ts
@@ -21,9 +21,8 @@ const reviews = [
     },
 ]
 
-// Solution
-function showReviewTotal (value : number) {
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString()
+function showReviewTotal (value : number, reviewer: string) {
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer
 }
 
-showReviewTotal(reviews.length)
+showReviewTotal(reviews.length, reviews[0].name)
