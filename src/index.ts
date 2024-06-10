8cce0d108bdc29aeeefe2196dd426d01574a84b0
Franz <franzj.pretorius.2003@gmail.com>
2024-06-10 13:53:20 +0200

    Fixing website mini challenge

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
 
 const reviews = [
@@ -27,3 +29,18 @@ function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean)
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+const you = {
+    userName: 'Bobby',
+    isReturning: true,
+}
+
+
+function populateUser(isReturning : boolean, userName : string ) {
+    if (isReturning){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName)
