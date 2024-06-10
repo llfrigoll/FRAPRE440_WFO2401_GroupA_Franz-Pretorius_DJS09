478ae5018f420008832c8cea1113308df8b18b38
Franz <franzj.pretorius.2003@gmail.com>
2024-06-10 14:02:24 +0200

    Array mini challenge

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,12 @@ const reviewTotalDisplay = document.querySelector('#reviews')
 
 let isOpen : boolean
 
-const reviews = [
+const reviews : {
+    name: string;
+    stars: number;
+    loyaltyUser: boolean;
+    date: string;
+}[] = [
     {
         name: 'Sheia',
         stars: 5,
